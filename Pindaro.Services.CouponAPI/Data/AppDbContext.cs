70d9689ad7dd8e5a23ce4b34ca95fd40442f1aaa
Luan Lobo Assis <luan2lobo@gmail.com>
2023-09-17 22:29:18 -0300

    Coupon API

---
+++ Pindaro.Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pindaro.Services.CouponAPI.Models;
+
+namespace Pindaro.Services.CouponAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 1,
+                CouponCode = "10OFF",
+                DiscountAmount = 10,
+                MinAmount = 20,
+            });
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 2,
+                CouponCode = "20OFF",
+                DiscountAmount = 20,
+                MinAmount = 30,
+            });
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 3,
+                CouponCode = "30OFF",
+                DiscountAmount = 30,
+                MinAmount = 40,
+            });
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 4,
+                CouponCode = "40OFF",
+                DiscountAmount = 40,
+                MinAmount = 50,
+            });
+
+        }
+    }
+}
---
+++ Pindaro.Services.CouponAPI/MappingConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Pindaro.Services.CouponAPI.Models;
+using Pindaro.Services.CouponAPI.Models.Dto;
+
+namespace Pindaro.Services.CouponAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<CouponDto, Coupon>().ReverseMap();
+            });
+            return mappingConfig;
+        }
+    }
+}
