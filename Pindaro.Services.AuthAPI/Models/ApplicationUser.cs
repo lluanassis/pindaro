6839ed6612944fb6e20162f92de642a4c4a89359
Luan Lobo Assis <luan2lobo@gmail.com>
2023-09-22 10:57:51 -0300

    Campo Name no ApplicationUser

---
+++ Pindaro.Services.AuthAPI/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Pindaro.Services.AuthAPI.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+    }
+}
