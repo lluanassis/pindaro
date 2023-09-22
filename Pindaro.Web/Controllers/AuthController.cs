e699ed0a3e471b048a1acfc6768dd437bf9b125c
Luan Lobo Assis <luan2lobo@gmail.com>
2023-09-22 17:57:19 -0300

    Login in Action

--- Pindaro.Web/Controllers/AuthController.cs
+++ Pindaro.Web/Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
 using Pindaro.Web.Models;
 using Pindaro.Web.Services.IService;
 using Pindaro.Web.Utility;
@@ -19,6 +20,25 @@ namespace Pindaro.Web.Controllers
         {
             LoginRequestDto loginRequestDto = new();
             return View(loginRequestDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginRequestDto obj)
+        {
+            ResponseDto responseDto = await _authService.LoginAsync(obj);
+
+            if (responseDto != null && responseDto.IsSuccess)
+            {
+                LoginResponseDto loginResponseDto =
+                    JsonConvert.DeserializeObject<LoginResponseDto>(Convert.ToString(responseDto.Result));
+
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("CustomError", responseDto.Message);
+                return View(obj);
+            }
         }
 
         [HttpGet]
