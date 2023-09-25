2fc5af2f5cf89c396f9506c22aac602adf6c68fa
Luan Lobo Assis <luan2lobo@gmail.com>
2023-09-25 11:48:20 -0300

    Product API

---
+++ Pindaro.Web/Services/IService/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using Pindaro.Web.Models;
+
+namespace Pindaro.Web.Services.IService
+{
+    public interface IProductService
+    {
+        Task<ResponseDto?> GetProduct(string couponCode);
+        Task<ResponseDto?> GetAllProductsAsync();
+        Task<ResponseDto?> GetProductByIdAsync(int id);
+        Task<ResponseDto?> CreateProductsAsync(ProductDto couponDto);
+        Task<ResponseDto?> UpdateProductsAsync(ProductDto couponDto);
+        Task<ResponseDto?> DeleteProductsAsync(int id);
+    }
+}
